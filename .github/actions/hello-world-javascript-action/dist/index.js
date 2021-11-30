1a44b216308f124fd6c507e46256c5868b270447
cjuan-code <cjuaniestacio@gmail.com>
2021-11-30 17:25:37 +0100

    change dist action

--- .github/actions/hello-world-javascript-action/dist/index.js
+++ .github/actions/hello-world-javascript-action/dist/index.js
@@ -8448,12 +8448,12 @@ const github = __nccwpck_require__(7364);
 try  {
     // “who-to-greet' input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello S(nameToGreet)!`);
+    console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workf low
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: $S(payload)`);
+    console.log(`The event payload: ${payload}`);
 
 } catch (error) {
     core.setFailed(error.message);
