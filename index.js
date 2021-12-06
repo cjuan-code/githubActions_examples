f5e79c79f045c5ba1dedd01aa0f1962cd19b2187
cjuan-code <cjuaniestacio@gmail.com>
2021-12-06 22:49:22 +0100

    test result modify readme

---
+++ index.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var core = require('@actions/core')
+
+var frase_positiva = core.getInput("frase_positiva");
+var frase_negativa = core.getInput("frase_negativa");
+var result = core.getInput("resultado_tests");
+
+if (result == 'sucess') {
+    fs.writeFileSync('README.md', '# ' + frase_positiva);
+} else if (result == 'failure') {
+    fs.writeFileSync('README.md', '# ' + frase_negativa);
+}
+
+console.log('Meme añadido al readme');
