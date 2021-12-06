5183049d1697bad6e11e2b39b4e4bb81b141001e
cjuan-code <cjuaniestacio@gmail.com>
2021-12-06 19:32:14 +0100

    send telegram message workflow

---
+++ index.js
@@ -0,0 +1,21 @@
+const TelegramBot = require('node-telegram-bot-api');
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const token = process.env.TG_TOKEN;
+const chatId = process.env.TG_CHAT_ID;
+const person = process.env.PERSON;
+
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+async function sendTGMessage() {
+  
+  await bot.sendMessage(chatId, 'Workflow ejecutado correctamente tras el último commit. Saludos '+ person + '.');
+  console.log('Mensaje enviado');
+
+  process.exit(0);
+}
+
+sendTGMessage();
